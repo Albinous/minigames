24b3caf7e182f216e9b9e61aee1433ac9d2dc23e
Albina Kadyrgali <albina200235@gmail.com>
2026-09-16 19:04:06 +0500

    feat: add players into the table

--- src/features/leaderboard/leaderboard.ts
+++ src/features/leaderboard/leaderboard.ts
@@ -1,10 +1,12 @@
 import { getLeaderBoardView } from "./leaderboard.view";
+import playersData from '../../data/leaderboard.json'
 
 export class LeaderBoard {
   public render(): HTMLElement {
+    const players = playersData.data;
     const leaderboard = document.createElement('section');
     leaderboard.className = 'leaderboard';
-    leaderboard.innerHTML = getLeaderBoardView();
+    leaderboard.innerHTML = getLeaderBoardView(players);
 
     return leaderboard;
   }
--- src/features/leaderboard/leaderboard.view.ts
+++ src/features/leaderboard/leaderboard.view.ts
@@ -1,7 +1,38 @@
-export function getLeaderBoardView(): string {
+import type { ILeaderboard } from "../../core"
+
+export function getLeaderBoardView(players: ILeaderboard[]): string {
   return `
     <div class="container">
       <h2 class="section-title">Top Players This Week</h2>
+      <table class="leaderboard-tabel">
+        <thead class="leaderboard-head">
+          <tr class="leaderboard-head__row">
+            <th scope="col" class="leaderboard-head__title">Rank</th>
+            <th scope="col" class="leaderboard-head__title">Player</th>
+            <th scope="col" class="leaderboard-head__title">Games Played</th>
+            <th scope="col" class="leaderboard-head__title">Total Score</th>
+            <th scope="col" class="leaderboard-head__title">Streak</th>
+            <th scope="col" class="leaderboard-head__title">Favorite Game</th>
+          </tr>
+        </thead>
+
+            <tbody class="leaderboard-players">
+              ${players.map(playerData => getPlayerView(playerData)).join('')}
+            </tbody>
+      </table>
     </div>
+  `
+}
+
+function getPlayerView(player: ILeaderboard): string {
+  return `
+    <tr class="leaderboard-player">
+      <td class="leaderboard-player__rank">${player.rank}</td>
+      <td class="leaderboard-player__name">${player.playerName}</td>
+      <td class="leaderboard-player__games">${player.gamesPlayed}</td>
+      <td class="leaderboard-player__score">${player.totalScore}</td>
+      <td class="leaderboard-player__streak">🔥 ${player.streakDays}</td>
+      <td class="leaderboard-player__favourite">${player.favoriteGameName}</td>
+    </tr>
   `
 }

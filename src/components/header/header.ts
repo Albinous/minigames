7319b2b88854284ba0aadc0743865f56996905eb
Albina Kadyrgali <albina200235@gmail.com>
2026-09-18 11:29:17 +0500

    feat: add logo component

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,3 +1,4 @@
+import { Logo } from '../logo';
 import { getHeaderView } from './header.view';
 
 export class Header {
@@ -6,6 +7,11 @@ export class Header {
 
     header.className = 'header';
     header.innerHTML = getHeaderView();
+
+    const headerWrapper = header.querySelector('.header-wrapper');
+    const logo: HTMLElement = new Logo().render();
+
+    headerWrapper?.prepend(logo);
 
     return header;
   }

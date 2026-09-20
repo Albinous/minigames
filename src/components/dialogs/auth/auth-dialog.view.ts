a91512c3513aa79e6262862d10dfc16fc1929a04
Albina Kadyrgali <albina200235@gmail.com>
2026-09-21 03:44:08 +0500

    feat: add auth login form

--- src/components/dialogs/auth/auth-dialog.view.ts
+++ src/components/dialogs/auth/auth-dialog.view.ts
@@ -20,9 +20,74 @@ export function getAuthDialogView(mode: AuthMode): string {
         </div>
 
         <div class="auth-dialog__content">
-          ${mode === 'login' ? 'Login' : 'Register'}
+          ${getLoginFormView()}
         </div>
       </div>
     </div>
+  `;
+}
+
+function getLoginFormView(): string {
+  return `
+    <form class="auth-form">
+      <h2>Welcome Back!</h2>
+
+      <p>
+        Sign in to resume your games and progress.
+      </p>
+
+      <div class="auth-form__field">
+        <label for="login-email">
+          Email Address
+        </label>
+
+        <input
+          id="login-email"
+          name="email"
+          type="email"
+          placeholder="e.g. alex@minigames.com"
+        />
+      </div>
+
+      <div class="auth-form__field">
+        <label for="login-password">
+          Password
+        </label>
+
+        <input
+          id="login-password"
+          name="password"
+          type="password"
+          placeholder="••••••••"
+        />
+      </div>
+
+      <a href="#">Forgot Password?</a>
+
+      <button
+        class="btn btn-primary auth-form__submit"
+        type="submit"
+      >
+        Login
+      </button>
+
+      <div class="auth-form__divider">
+        <span>OR</span>
+      </div>
+
+      <button
+        type="button"
+        class="auth-form__google"
+      >
+        Continue with Google
+      </button>
+
+      <p>
+        Don't have an account?
+        <button type="button">
+          Register
+        </button>
+      </p>
+    </form>
   `;
 }

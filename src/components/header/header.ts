f3067268fec2d6525e9ba3b9b892aae0346549db
Albina Kadyrgali <albina200235@gmail.com>
2026-09-21 02:15:08 +0500

    feat: implement burger menu into header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -3,26 +3,38 @@ import { BurgerMenu } from './burger-menu';
 import { getHeaderView } from './header.view';
 
 export class Header {
-  public render(): HTMLElement {
+   private readonly burgerMenu: BurgerMenu;
+
+  constructor() {
+    this.burgerMenu = new BurgerMenu();
+  }
+  private addLogo(header: HTMLElement): void {
+    const headerWrapper = header.querySelector('.header-wrapper');
+
+    headerWrapper?.prepend(new Logo().render());
+  }
+
+  private addBurgerMenu(header: HTMLElement): void {
+    header.append(this.burgerMenu.render());
+  }
+
+  private bindEvents(header: HTMLElement): void {
+    const burgerButton = header.querySelector('.header-burger');
+
+    burgerButton?.addEventListener('click', () => {
+      this.burgerMenu.open();
+    });
+  }
+
+    public render(): HTMLElement {
     const header = document.createElement('header');
 
     header.className = 'header';
     header.innerHTML = getHeaderView();
 
-    const headerWrapper = header.querySelector('.header-wrapper');
-    const logo = new Logo();
-
-    headerWrapper?.prepend(logo.render());
-
-    const burgerMenu = new BurgerMenu();
-
-    document.body.append(burgerMenu.render());
-
-    const burgerButton = header.querySelector('.header-burger');
-
-    burgerButton?.addEventListener('click', () => {
-      burgerMenu.open();
-    });
+    this.addLogo(header);
+    this.addBurgerMenu(header);
+    this.bindEvents(header);
 
     return header;
   }
